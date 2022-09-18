91c7680603e2f7205a8896c6ae1ac9762d734b8b
Jacek <jacek.barszczewski@gmail.com>
2022-09-18 17:52:37 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from search_paper import Win1
+import tkinter as tk
+
+def main(): 
+    master = tk.Tk()
+    app = Win1(master)
+    master.mainloop()
+    
+if __name__ == '__main__':
+    main()
